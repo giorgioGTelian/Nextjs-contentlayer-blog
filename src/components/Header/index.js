bba5d8b7ab2b92d40411e01708eeffec4709c58b
giorgio <giorgio.programmer@gmail.com>
2023-10-07 14:40:35 +0200

    header done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,27 @@
+import {LinkedinIcon, GithubIcon, SunIcon} from '../Icons';
+import Logo from './logo';
+import Link  from 'next/link';
+
 const Header = () => {
     return (
     <header className="w-full p-4 px-10 flex items-center justify-between">
-        <h1>My Header</h1>
+        <Logo />
+        <div>
+            <a href='http://' className='inline-block w-6 h-6 mr-4'>
+                <LinkedinIcon className=" hover:scale-125  transi ease duration-200"/></a>
+            <a href='http://' className='inline-block w-6 h-6 mr-4'>
+                <GithubIcon className=" hover:scale-125 transi ease duration-200"/></a>
+        </div>
+        <nav className='w-max py-3 px-8 border border-solid 
+        border-dark rounded-full font-medium capitalize flex 
+        items-center bg-light/80 backdrop-blur-sm '>
+            <Link href="/" className='mr-2'>Home</Link>
+            <Link href="/about" className='mr-2'>About</Link>
+            <Link href="/contact" className='mr-2'>Contact</Link>
+            <button>
+                <SunIcon className="w-6 h-6" />
+            </button>
+        </nav>
     </header>
     );
 };
--- src/components/Header/logo.js
+++ src/components/Header/logo.js
@@ -5,10 +5,11 @@ import profileImg from "@/public/profile-img.png"
 
 const logo = () => {
     return (
-        <Link href={"/"}>
-            <div className="flex items-center justify-center">
-                <Image src={profileImg} alt="Giorgio" className='w-full h-auto rounded-full' />
+        <Link href={"/"} className='flex items-center text-dark'>
+            <div className="justify-center w-16 overflow-hidden border-solid border-dark mr-4">
+                <Image src={profileImg} alt="Luna" className='w-full h-auto rounded-full' />
             </div>
+            <span className='font-bold text-xl'>Luna Programming Language</span>
         </Link>
     )
 }
