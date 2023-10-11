a058028a34cae609f05b8fff2084d049bb4657ed
giorgio <giorgio.programmer@gmail.com>
2023-10-11 17:40:30 +0200

    sdasd

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -1,4 +1,5 @@
 import { sortBlogs } from '@/src/utils';
+import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react'
 
@@ -11,25 +12,41 @@ const FeaturedPosts = ({blogs}) => {
             </h2>
             <div className='grid grid-cols-2 grid-rows-2 gap-6 mt-16'>
                 <article className='flex flex-col items-start justify-start'>
-                    <Image src={sortedBlogs[0].image.filePath.replace("../public", "")} 
+                    <Image src={sortedBlogs[1].image.filePath.replace("../public", "")} 
                     alt={blogs.title}
                     placeholder={'blur'}
-                    blurDataURL={sortedBlogs[0].image.blurhashDataUrl}
+                    blurDataURL={sortedBlogs[1].image.blurhashDataUrl}
                     className='w-full h-full object-center object-cover rounded-3xl -z-10'
                     width={500}
                     height={500}
                     />
+                    <Link href={`/blog/${sortedBlogs[1].url}`}>
                     <h3 className='text-2xl font-bold'>
-                        {sortedBlogs[1].title}
+                        <span className='bg-gradient-to-r from-accent to-accent bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500'>
+                            {sortedBlogs[1].title}
+                        </span>
                     </h3>
+                    </Link>
                     <p className='text-xl mt-4'>
                         {sortedBlogs[1].description}
                     </p>
                 </article>
                 <article className='flex flex-col items-start justify-start'>
+                    <Image src={sortedBlogs[2].image.filePath.replace("../public", "")}
+                    alt={blogs.title}
+                    placeholder={'blur'}
+                    blurDataURL={sortedBlogs[2].image.blurhashDataUrl}
+                    className='w-full h-full object-center object-cover rounded-3xl -z-10'
+                    width={500}
+                    height={500}
+                    />
+                    <Link href={`/blog/${sortedBlogs[2].url}`}>
                     <h3 className='text-2xl font-bold'>
-                        {sortedBlogs[2].title}
+                        <span className='bg-gradient-to-r from-accent to-accent bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500'>
+                            {sortedBlogs[2].title}
+                        </span>
                     </h3>
+                    </Link>
                     <p className='text-xl mt-4'>
                         {sortedBlogs[2].description}
                     </p>
