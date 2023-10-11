637c38dfb5c3313729b129b808d2107e712c2652
giorgio <giorgio.programmer@gmail.com>
2023-10-11 17:20:30 +0200

    adsadas

--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -1,8 +1,32 @@
+import { sortBlogs } from '@/src/utils';
 import React from 'react'
 
 const FeaturedPosts = ({blogs}) => {
+    const sortedBlogs = sortBlogs(blogs);
     return (
-        <div>FeaturedPosts</div>
+        <section className='w-full mt-32 px-32 flex flex-col items-center justify-center'>
+            <h2 className='w-full inline-block font-bold text-4xl'>
+                Featured Posts
+            </h2>
+            <div className='grid grid-cols-2 grid-rows-2 gap-6 mt-16'>
+                <article className='flex flex-col items-start justify-start'>
+                    <h3 className='text-2xl font-bold'>
+                        {sortedBlogs[1].title}
+                    </h3>
+                    <p className='text-xl mt-4'>
+                        {sortedBlogs[1].description}
+                    </p>
+                </article>
+                <article className='flex flex-col items-start justify-start'>
+                    <h3 className='text-2xl font-bold'>
+                        {sortedBlogs[2].title}
+                    </h3>
+                    <p className='text-xl mt-4'>
+                        {sortedBlogs[2].description}
+                    </p>
+                </article>
+            </div>
+        </section>
     )
 }
 
