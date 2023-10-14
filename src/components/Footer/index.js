44e6420bd35a7ed36074aab21726a0dfe0511709
giorgio <giorgio.programmer@gmail.com>
2023-10-14 21:57:47 +0200

    dasd

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,17 @@
+"use client"
 import React from 'react'
-import Link from 'next/link';
 import { ArrowUp } from '../Icons';
+import { useForm } from 'react-hook-form';
 
 const Footer = ({scrollToTop}) => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
     
     return (
-        <footer className='w-full mt-32 px-32 flex flex-col items-center justify-center'>
+        <footer className='w-full rounded-2xl mt-16 px-16 flex flex-col items-center justify-center bg-dark text-light'>
             <div className='flex w-full justify-between'>
-                <h3 className='inline-block font-bold text-4xl'>
+                <h3 className='inline-block font-bold text-4xl mt-8'>
                 Interesting Stories | Updates | Guides
                 </h3>
                 
@@ -16,11 +20,18 @@ const Footer = ({scrollToTop}) => {
                     <ArrowUp />
                 </button>
             </div>
-                <p className='text-xl mt-4'>
-                Subscribe to learn about new technology and updates. Join over 5000+ members community to stay up to date with latest news.
-                © 2023 CodeBucks. All rights reserved.
-                Made with ❤ by GTElian
+                <p className='text-xl mt-4 px-4'>
+                Subscribe to learn about new technology and updates. Join over 5000+ members community to stay up to date with latest news.<br />
+                © 2023 CodeBucks. All rights reserved.<br />
+                Made with ❤ by GTElian<br />
                 </p>
+                <form onSubmit={handleSubmit(onSubmit)} className='mt-6 min-w-[384px] flex items-stretch bg-light p-2 rounded mx04'>
+                    <input type="text" placeholder="Name" {...register("Name", {required: true})} />
+                    <input type="email" placeholder="Email " {...register("Email ", {required: true})} />
+
+                    <input type="submit" placeholder='Send'/>
+                    </form>
+
             </footer>
     )
 }
