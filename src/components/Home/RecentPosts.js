0322b93da81f7e5744ab1ddb70bb08982c0c8585
giorgio <giorgio.programmer@gmail.com>
2023-10-14 10:27:23 +0200

    sdasda

--- src/components/Home/RecentPosts.js
+++ src/components/Home/RecentPosts.js
@@ -7,10 +7,42 @@ const RecentPosts = ({blogs}) => {
     const sortedBlogs = sortBlogs(blogs);
     return (
         <section className='w-full mt-32 px-32 flex flex-col items-center justify-center'>
-            <h2 className='w-full inline-block font-bold text-4xl'>
-                Recent Posts
-            </h2>
-            
+            <div className='flex w-full justify-between'>
+                <h2 className='inline-block font-bold text-4xl'>
+                    Recent Posts
+                </h2>
+                <button className='inline-block font-medium text-accent text-lg'>view all
+                    <Link href="/categories/all"/>
+                </button>
+            </div>
+
+            <div className='grid grid-cols-3 grid-rows-3 gap-16 mt-16'>
+                {
+                    sortedBlogs.slice(5, 11).map((blog, index) => {
+                        <article className='group flex flex-col items-start justify-start'>
+                            <Image src={blog.image.filePath.replace("../public", "")} 
+                            alt={blog.title}
+                            placeholder={'blur'}
+                            blurDataURL={blog.image.blurhashDataUrl}
+                            className='w-full h-full object-center object-cover rounded-3xl -z-10'
+                            width={500}
+                            height={500}
+                            />
+                            <Link href={`/blog/${blog.url}`}>
+                            <h3 className='text-2xl font-bold'>
+                                <span className='bg-gradient-to-r from-accent to-accent bg-[length:0px_6px] hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500'>
+                                    {blog.title}
+                                </span>
+                            </h3>
+                            </Link>
+                            <p className='text-xl mt-4 invisible group-hover:visible'>
+                                {blog.description}
+                            </p>
+                        </article>
+                    })
+
+                };
+            </div>
         </section>
     )
 }
