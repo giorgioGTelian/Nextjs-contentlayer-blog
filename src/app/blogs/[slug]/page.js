670dd7d99d75ddda8c9a2c3f548246a72aa3d3ab
giorgio <giorgio.programmer@gmail.com>
2023-10-20 08:05:37 +0200

    sadda

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -1,6 +1,9 @@
+import { allBlogs } from "@/.contentlayer/generated"
 
 
 export default function BlogPage({ params }) {
     
+    const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug)
+
     return <div>Post: {params.slug}</div>
 }
